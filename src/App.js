3166ecf36f3221c77e7ec260b990c849af53053a
k-dot-11 <karnatheveer@gmail.com>
2020-07-12 17:26:27 +0530

    API support added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const key = '32bbb72dd3f89e74578605f98eff3b31';
+
+	const getData = () => {
+		fetch('https://cors-anywhere.herokuapp.com/v2.api-football.com/leagues/', {
+			headers: {
+        'X-RapidAPI-Key': '32bbb72dd3f89e74578605f98eff3b31',
+        
+			}
+		})
+			.then((response) => response.json())
+			.then((data) => {
+        for(var i =0;i<25;++i){
+          console.log(data.api.leagues[i])
+        }
+      })
+			.catch((error) => console.log(error));
+	};
+
+	return (
+		<div className="App">
+			<Navbar bg="light" expand="md" className="navbar">
+				<Navbar.Brand href="#home" className="navbrand">
+					The Score Story
+				</Navbar.Brand>
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="mr-auto">
+						<Nav.Link href="#home" className="navlinks">
+							Home
+						</Nav.Link>
+						<Nav.Link href="#link" className="navlinks">
+							About
+						</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+			<br />
+			<Button variant="primary" onClick={getData}>
+				GET DATA
+			</Button>
+		</div>
+	);
 }
 
 export default App;
