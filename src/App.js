a1dfd27ebc55e39b0d7cc982ff308ba7aa339939
k-dot-11 <karnatheveer@gmail.com>
2020-07-13 00:42:54 +0530

    Added teams list with images

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
-import { Navbar, Nav, NavDropdown, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Nav, NavDropdown, Form, FormControl, Button, Container, ListGroup, Image } from 'react-bootstrap';
 
 function App() {
 	const key = '32bbb72dd3f89e74578605f98eff3b31';
 
+	const [ teamList, setTeamList ] = useState([]);
+	const [ isLoading, setIsLoading ] = useState(false);
+
 	const getData = () => {
-		fetch('https://cors-anywhere.herokuapp.com/v2.api-football.com/leagues/', {
+		setIsLoading(true);
+		const list = [];
+		fetch('https://cors-anywhere.herokuapp.com/v2.api-football.com/teams/league/524', {
 			headers: {
-        'X-RapidAPI-Key': '32bbb72dd3f89e74578605f98eff3b31',
-        
+				'X-RapidAPI-Key': '32bbb72dd3f89e74578605f98eff3b31'
 			}
 		})
 			.then((response) => response.json())
 			.then((data) => {
-        for(var i =0;i<25;++i){
-          console.log(data.api.leagues[i])
-        }
-      })
+				for (var i = 0; i < 20; ++i) {
+					list.push(data.api.teams[i]);
+					console.log(data.api.teams[i]);
+				}
+
+				setTeamList(list);
+				setIsLoading(false);
+			})
 			.catch((error) => console.log(error));
 	};
 
+	const listItems = teamList.map((item) => (
+		<ListGroup.Item key={item.team_id}>
+			<Image src={item.logo} style={{height:'20px'}} roundedCircle />
+			{item.name}
+		</ListGroup.Item>
+	));
+
+	if (isLoading) return <Container fluid>LOADING...</Container>;
+
 	return (
-		<div className="App">
+		<Container fluid className="App">
 			<Navbar bg="light" expand="md" className="navbar">
 				<Navbar.Brand href="#home" className="navbrand">
 					The Score Story
@@ -39,10 +56,11 @@ function App() {
 				</Navbar.Collapse>
 			</Navbar>
 			<br />
-			<Button variant="primary" onClick={getData}>
+			<ListGroup>{listItems}</ListGroup>
+			<Button variant="dark" onClick={getData}>
 				GET DATA
 			</Button>
-		</div>
+		</Container>
 	);
 }
 
